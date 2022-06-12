927d412b1ae4f61a9da4d76e23f1eb8f87d4ca3b
Fernando Bártolo <git@fbart.net>
2022-06-12 19:01:58 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,6 @@ except URLError as e:
 streamlit.stop()
 
 
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
